463f9b62bdd89164d2d718f48a904531ffc892b1
Gábor Horváth <hungaborhorvath@gmail.com>
2025-11-15 20:04:55 +0100

    Add solution for 2024/01 (both parts)

---
+++ 2024/01/aoc_01.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+def parse_input(input_data: str) -> list[list[int], list[int]]:
+    l = [[], []]
+    merged_numbers = input_data.split()
+    for i, nr in enumerate(merged_numbers):
+        l[i%2].append(int(nr))
+    return l
+
+def differences(l1: list[int], l2: list[int]) -> list[int]:
+    l = []
+    for a, b in zip(sorted(l1), sorted(l2)):
+        l.append(abs(a-b))
+    return l
+
+def similarity_score(l1: list[int], l2: list[int]) -> list[int]:
+    return sum(a*l2.count(a) for a in l1)
+
+def main(file_name: str):
+    with open(file_name) as input_file:
+        input_data = input_file.read()
+    l1, l2 = parse_input(input_data)
+    answer = (sum(differences(l1, l2)), similarity_score(l1, l2))
+    return answer
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[-1]))
+
