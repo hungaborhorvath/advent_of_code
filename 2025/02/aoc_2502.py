f6743d7525093837352c8a12ff4f62437f207e46
Gábor Horváth <hungaborhorvath@gmail.com>
2025-12-02 06:55:52 +0100

    Add (a bit slow) solution for 2025/02 (both parts)

---
+++ 2025/02/aoc_2502.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def parse_input(input_data: str) -> list[tuple[int], [int]]:
+    ranges = []
+    for range_string in input_data.split(','):
+        ranges.append(tuple(int(n) for n in range_string.split('-')))
+    return ranges
+
+
+def is_invalid_id(n: int, d:int =2) -> bool:
+    s = str(n)
+    length = len(s)
+    if length % d != 0:
+        return False
+    else:
+        pattern_length = length // d
+        return len(set(s[i*pattern_length:(i+1)*pattern_length]
+                       for i in range(d))) == 1
+
+
+def invalid_ids_in_range(start:int, end:int) -> list[int]:
+    invalid_ids = []
+    ## naive approach to iterate through all ids
+    ## slow, but manageable
+    for n in range(start, end+1):
+        if is_invalid_id(n):
+            invalid_ids.append(n)
+    return invalid_ids
+
+def all_invalid_ids_in_range(start:int, end:int) -> list[int]:
+    invalid_ids = []
+    ## naive approach to iterate through all ids and potential pattern lengths
+    ## quite slow, but still manageable
+    for n in range(start, end+1):
+        for d in range(2, len(str(n)) + 1):
+            if is_invalid_id(n, d):
+                invalid_ids.append(n)
+                break
+    return invalid_ids
+
+
+def invalid_ids_in_input(ranges:list) -> list[int]:
+    invalid_ids = [invalid_ids_in_range(start, end) for (start, end) in ranges]
+    return sum(invalid_ids, start=[])
+
+def all_invalid_ids_in_input(ranges:list) -> list[int]:
+    all_invalid_ids = [all_invalid_ids_in_range(start, end)
+                       for (start, end) in ranges]
+    return sum(all_invalid_ids, start=[])
+
+
+def main(file_name: str):
+    with open(file_name) as input_file:
+        input_data = input_file.read()
+    ranges = parse_input(input_data)
+    invalid_ids = invalid_ids_in_input(ranges)
+    all_invalid_ids = all_invalid_ids_in_input(ranges)
+    answer = (sum(invalid_ids), sum(all_invalid_ids))
+    return answer
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[-1]))
