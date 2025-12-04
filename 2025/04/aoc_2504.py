d82984abf480a0f9b9978d2fac8c653fc23ac4e6
Gábor Horváth <hungaborhorvath@gmail.com>
2025-12-04 08:54:27 +0100

    Add (a bit slow) solution for 2025/04 (both parts)

---
+++ 2025/04/aoc_2504.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def parse_input(input_data:str) -> list[list[bool]]:
+    lines = [line for line in input_data.split('\n') if line]
+    rolls = [[True if s == '@' else False for s in line] for line in lines]
+    return rolls
+
+
+def accessible_roll(rolls:list[list[bool]], i:int, j:int, maxi:int, maxj:int,
+                    mini=0, minj=0) -> bool:
+    neighbors = [(i-1, j-1), (i-1, j), (i-1, j+1), (i, j-1), (i, j+1),
+                  (i+1, j-1), (i+1, j), (i+1, j+1)]
+    valid_neighbors = [(k, l) for (k, l) in neighbors
+                       if (k >= mini and k < maxi)
+                       and (l >= minj and l < maxj)]
+    valid_neighbor_rolls = [(k, l) for (k, l) in valid_neighbors
+                            if rolls[k][l]]
+    return rolls[i][j] and len(valid_neighbor_rolls) < 4
+
+def accessible_rolls(rolls) -> list[tuple[int, int]]:
+    maxi = len(rolls)
+    maxj = len(rolls[0])
+    l = []
+    for i in range(maxi):
+        for j in range(maxj):
+            if accessible_roll(rolls, i, j, maxi, maxj):
+                l.append((i, j))
+    return l
+
+
+def rolls_after_remove(rolls:list[list[bool]],
+                       removeables:list[tuple[int, int]]) -> list[list[bool]]:
+    new_rolls = rolls.copy()
+    for (i, j) in removeables:
+        new_rolls[i][j] = False
+    return new_rolls
+
+def number_of_removeable_rolls(rolls:list[list[bool]]) -> int:
+    s = 0
+    new_rolls = rolls.copy()
+    while accessible_rolls(new_rolls):
+        s += len(accessible_rolls(new_rolls))
+        new_rolls = rolls_after_remove(new_rolls, accessible_rolls(new_rolls))
+    return s
+
+
+def main(file_name:str):
+    with open(file_name) as input_file:
+        input_data = input_file.read()
+    rolls = parse_input(input_data)
+    answer = (len(accessible_rolls(rolls)), number_of_removeable_rolls(rolls))
+    return answer
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[-1]))
